249b2c5bcd5f32a58cca41dc4b7e05b58d747bac
iamdipak39p <iamdipak39@gmail.com>
2022-06-24 15:07:19 +0530

    commited

---
+++ ActiveTraders.py
@@ -0,0 +1,18 @@
+customers=["z","aaa","bbb","bbb","bbb","bbb","bbb","bbb","cc","cc","cc","cc","cc","cc","cc","cc","cc","cc","cc","cc","cc","cc","cc"]
+print(sorted(customers))
+cust=[]
+per=[]
+Activetraders=[]
+i=0
+for each in customers:
+    if each not in cust:
+        cust.append(each)
+
+
+for each in cust:
+    per.append(customers.count(each)/len(customers)*100)
+
+for i in range(0,len(cust)):
+    if per[i]>5:
+        Activetraders.append(cust[i])
+
